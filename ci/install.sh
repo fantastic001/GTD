f4f98520cad4c3825d4d798f254a8c417ebada50
Stefan Nožinić <stefan@lugons.org>
2023-11-18 12:04:18 +0100

    Setup jira auth

--- ci/install.sh
+++ ci/install.sh
@@ -6,3 +6,14 @@ virtualenv ./env
 . env/bin/activate 
 
 pip install -r requirements.txt 
+mkdir -p ~/.config 
+
+echo "
+{
+	\"url\": \"$my_jira_url\",
+	\"username\": \"$my_jira_username\",
+	\"password\": \"$my_jira_password\"
+}
+
+
+" > ~/.config/gtd.json
