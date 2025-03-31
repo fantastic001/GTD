e59a908d52e8e45bcfed0b18a9cae229d27fda59
Stefan Nožinić <stefan@lugons.org>
2025-03-31 20:41:51 +0200

    Add maintenance module for handling maintenance tasks

---
+++ gtd/maintenance.py
@@ -0,0 +1,36 @@
+
+from gtd.extensions import Report
+from gtd.config import get_config_str, get_config_bool
+from gtd.drive import Spreadsheet
+import datetime
+from gtd.style import section, table, paragraph, items
+
+def add_extensions(report: Report):
+    """
+    Reads table for maintenance and adds items with unmaintained items
+    """
+    import pandas as pd 
+    maintenance_file_path = get_config_str("maintenance_file", "", "Path to the maintenance file")
+    sheet_name = get_config_str("maintenance_sheet", "maintenance", "Sheet name in the maintenance file")
+    last_maintenance_column = get_config_str("last_maintenance_column", "Last maintenance", "Column name for last maintenance")
+    frequency_column = get_config_str("frequency_column", "Frequency (days)", "Column name for frequency")
+    item_column = get_config_str("item_column", "Item", "Column name for item")
+    operation_column = get_config_str("operation_column", "Operation", "Column name for operation")
+    if maintenance_file_path == "":
+        return 
+    maintenance = Spreadsheet(maintenance_file_path.replace("drive://", ""))
+    df = maintenance.open_pandas(sheet_name)
+    if df is None:
+        return
+    report.add(section("Maintenance"))
+    df[last_maintenance_column] = df[last_maintenance_column].apply(pd.to_datetime)
+    df[frequency_column] = df[frequency_column].apply(int)
+    df[frequency_column] = df[frequency_column].apply(lambda x: datetime.timedelta(days=x))
+    df = df[df[last_maintenance_column] + df[frequency_column] < datetime.datetime.now()]
+    if df.empty:
+        report.add(paragraph("No maintenance required"))
+        return
+    for item, operations in df.groupby(item_column):
+        report.add(section(item, 1))
+        report.add(items(operations[operation_column].tolist()))
+    
