28179554f80c7f21320cb0d19bae7e72272519b7
Stefan Nožinić <stefan@lugons.org>
2025-05-09 14:17:35 +0200

    Refactor extension loading logic to improve error handling and clarity; replace wildcard import with specific imports and use parallel execution

--- gtd/extensions.py
+++ gtd/extensions.py
@@ -1,6 +1,8 @@
 
-from  gtd.config import * 
+from  gtd.config import get_symbols_satisfying
+from multiprocessing import Pool
 
+from gtd.style import paragraph, red
 class Report:
     def __init__(self) -> None:
         self.elements = [] 
@@ -11,7 +13,24 @@ class Report:
     def get_elements(self):
         return self.elements
 
-def load_extensions() -> list:
+def _extension_result_key(report):
+    return report.get_elements()[0]
+
+def is_extension(obj):
+    return hasattr(obj, "__name__") and obj.__name__ == "add_extensions" and callable(obj)
+def get_report(ext):
     report = Report()
-    notify_plugins("add_extensions", report)
-    return report.get_elements()
+    try:
+        ext(report)
+        return report
+    except Exception as e:
+        report.add(paragraph(red("Error in extension: " + str(e))))
+        return report
+def load_extensions() -> list:
+    extension_creators = get_symbols_satisfying(is_extension)
+    with Pool() as pool:
+        reports = pool.map(get_report, extension_creators)
+    result = []
+    for report in sorted(reports, key=_extension_result_key):
+        result.extend(report.get_elements())
+    return result
