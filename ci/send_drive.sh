c961bc2ff8b9357d9b49a5ac752c44e6963bc486
Stefan Nožinić <stefan@lugons.org>
2026-05-24 20:05:28 +0200

    Refactor send_drive.sh to conditionally copy reports and attachments based on WEEK_REPORTS variable

--- ci/send_drive.sh
+++ ci/send_drive.sh
@@ -2,4 +2,18 @@
 
 echo "Copying to $DRIVE"
 rclone copy report.html $DRIVE
-rclone copy ~/.gtd/attachments/* $DRIVE/Attachments
+
+if [ -n "$WEEK_REPORTS" ]; then
+    echo "Copying to $WEEK_REPORTS"
+    CURRENT_YEAR=$(date +%Y)
+    CALENDAR_WEEK=$(date +%V)
+    rclone copy report.html "$WEEK_REPORTS/$CURRENT_YEAR-$CALENDAR_WEEK"
+    if [ -d ~/.gtd/attachments ] && [ "$(ls -A ~/.gtd/attachments)" ]; then
+        rclone copy ~/.gtd/attachments/*  \
+            "$WEEK_REPORTS/$CURRENT_YEAR-$CALENDAR_WEEK/Attachments"
+    fi
+fi
+if [ -d ~/.gtd/attachments ] && [ "$(ls -A ~/.gtd/attachments)" ]; then
+    echo "Copying attachments to $DRIVE"
+    rclone copy ~/.gtd/attachments/* $DRIVE/Attachments
+fi
